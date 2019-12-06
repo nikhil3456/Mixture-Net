d42f425935cf8f2fae9910e85671f3d12dac614f
Nikhil Bansal <nikhilbansal3456@gmail.com>
2019-12-06 15:24:27 +0530

    Add file

--- property_prediction_ofm/model.py
+++ property_prediction_ofm/model.py
@@ -7,32 +7,27 @@ from torch.autograd import Variable
 class Net(nn.Module):
     def __init__(self, input_size=(1, 32, 32)):
         super(Net, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(1, 32, (5, 5)), # padding = (F-1)/2 (to keep the size constant) 
-            nn.ReLU(),
-            nn.Conv2d(32, 32, (3, 3)),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2), # 32
-            nn.Conv2d(32, 64, (3, 3)),
-            nn.ReLU(),
-        )
         
-        self.flat_fts = self.get_flat_fts(input_size, self.features)
+        self.conv1 = nn.Conv2d(1, 32, (5, 5)) # padding = (F-1)/2 (to keep the size constant) 
+        self.conv2 = nn.Conv2d(32, 32, (3, 3))
+        self.pool2 = nn.MaxPool2d(2, 2) # 32
+        self.conv3 = nn.Conv2d(32, 64, (3, 3))
         
-        self.classifier = nn.Sequential(
-            nn.Linear(self.flat_fts, 48),
-            nn.ReLU(),
-            nn.Linear(48, 32),
-            nn.ReLU(),
-            nn.Linear(32, 1)
-        )
+        self.fc1 = nn.Linear(7744, 48)
+        self.fc2 = nn.Linear(48, 32)
+        self.fc3 = nn.Linear(32, 1)
         
-    def get_flat_fts(self, in_size, fts):
-        f = fts(Variable(torch.ones(1,*in_size)))
-        return int(np.prod(f.size()[1:]))
+        self.relu = nn.ReLU(inplace=True)
     
     def forward(self, x):
-        fts = self.features(x)
-        flat_fts = fts.view(-1, self.flat_fts)
-        out = self.classifier(flat_fts)
-        return filterlat_fts
+        fts = self.relu(self.conv1(x))
+        fts = self.relu(self.conv2(fts))
+        fts = self.pool2(fts)
+        fts = self.relu(self.conv3(fts))
+        
+        flat_fts = fts.view(-1, 7744)
+        flat_fts = self.relu(self.fc1(flat_fts))
+        flat_fts = self.relu(self.fc2(flat_fts))
+        out_fts = flat_fts
+        out = self.fc3(flat_fts)
+        return out_fts
